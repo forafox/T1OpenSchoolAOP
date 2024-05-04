4a2d00cb75ead101c458092dcd86cec0755d8555
forafox <deskpa@yandex.ru>
2024-05-04 21:54:55 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("java")
+    id("io.spring.dependency-management") version "1.1.4"
+    id("org.springframework.boot") version "3.2.4"
+}
+
+group = "org.forafox"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.4")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+    implementation("org.springframework.boot:spring-boot-starter-aop:3.2.4")
+    implementation("org.projectlombok:lombok:1.18.28")
+    annotationProcessor("org.projectlombok:lombok")
+    runtimeOnly("org.postgresql:postgresql")
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
